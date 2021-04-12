be88e606ce25b7297012ebca8c1cba5babbd0512
chronoprocyon <82403768+chronoprocyon@users.noreply.github.com>
2021-04-12 20:24:44 +0300

    Update 002.py

--- 002.py
+++ 002.py
@@ -1,9 +1,9 @@
 seconds = int(input("Введите время в секундах >>> "))
 
-hours = seconds // 360
+hours = seconds // 3600
 
-minutes = (seconds % 360) // 60
+minutes = (seconds % 3600) // 60
 
-new_seconds = (seconds % 360) % 60
+new_seconds = (seconds % 3600) % 60
 
 print(f"{hours}:{minutes}:{new_seconds}")
