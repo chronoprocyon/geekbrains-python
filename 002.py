864cf0f58a5357222eefcdb9f4fd2776655785ce
chronoprocyon <82403768+chronoprocyon@users.noreply.github.com>
2021-04-12 20:16:46 +0300

    Delete 002.py

--- 002.py
+++
@@ -1,9 +0,0 @@
-seconds = int(input("Введите время в секундах >>> "))
-
-hours = seconds // 360
-
-minutes = (seconds % 360) // 60
-
-new_seconds = (seconds % 360) % 60
-
-print(f"{hours}:{minutes}:{new_seconds}")
